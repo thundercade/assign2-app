1d7828b5f3ff2fcfafd87a8ad942570dfa0354c9
thundercade <116746505+thundercade@users.noreply.github.com>
2022-11-29 15:08:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -44,9 +44,54 @@ st.write("You searched for:", text)
 st.write(corpus_embeddings.shape)
 st.write(df.shape)
 
+# changes started here
+
+embedder = SentenceTransformer('all-MiniLM-L6-v2')
+
+import torch
+
+
+# queries = ['Hotel in Shibuya near a ramen shop',
+#            'Hotel with large rooms and a good breakfast'
+#            ]
+
+queries = st.text_input("Describe the hotel you are looking for:")
+queries=list([queries])
+
+# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+top_k = min(5, len(corpus))
+for query in queries:
+    query_embedding = embedder.encode(query, convert_to_tensor=True)
+
+    # We use cosine-similarity and torch.topk to find the highest 5 scores
+    cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
+    top_results = torch.topk(cos_scores, k=top_k)
+
+    st.write("\n\n======================\n\n")
+    st.write("Query:", query)
+    st.write("\nTop 5 most similar sentences in corpus:")
+
+    for score, idx in zip(top_results[0], top_results[1]):
+        st.write("(Score: {:.4f})".format(score))
+        # st.write(corpus[idx], "(Score: {:.4f})".format(score))
+        row_dict = df.loc[df['all_review']== corpus[idx]]
+        st.write("paper_id:  " , row_dict['hotel'] , "\n")
+
+
+    # for idx, distance in results[0:closest_n]:
+    #     print("Score:   ", "(Score: %.4f)" % (1-distance) , "\n" )
+    #     print("Paragraph:   ", corpus[idx].strip(), "\n" )
+    #     row_dict = df.loc[df['all_review']== corpus[idx]]
+    #     print("paper_id:  " , row_dict['Hotel'] , "\n")
+    # """
+    # # Alternatively, we can also use util.semantic_search to perform cosine similarty + topk
+    # hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=5)
+    # hits = hits[0]      #Get the hits for the first query
+    # for hit in hits:
+    #     print(corpus[hit['corpus_id']], "(Score: {:.4f})".format(hit['score']))
+    # """
+
 st.write("You made it to the end!")
-
-
 #
 
 
